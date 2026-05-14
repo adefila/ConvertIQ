3454ff637bab86ca623b11fe8265496668d4ad59
Adefila Samuel Adeyemi <74382772+adefila@users.noreply.github.com>
2026-05-14 15:59:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: false },
+  experimental: {
+    serverActions: { allowedOrigins: ['*'] }
+  }
 }
 module.exports = nextConfig
