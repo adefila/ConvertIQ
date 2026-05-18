4f769ca74212084932248e9bd3b7e17bb8801107
Adefila Samuel Adeyemi <74382772+adefila@users.noreply.github.com>
2026-05-18 12:16:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,24 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: false },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'image.thum.io' },
+      { protocol: 'https', hostname: 'api.screenshotone.com' },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "img-src 'self' data: https://image.thum.io https://api.screenshotone.com;",
+          },
+        ],
+      },
+    ]
+  },
 }
 module.exports = nextConfig
